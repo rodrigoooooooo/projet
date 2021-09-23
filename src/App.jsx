92bc712c99ea6c133d39fb50b3a7027dba46b8c9
rodrigoooooooo <rodrigomelo310@gmail.com>
2021-09-23 12:26:33 -0300

    i

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,65 @@
 import React, { Component } from "react";
 import "./App.css";
-import axios from 'axios';
-
+import axios from "axios";
 
 class App extends Component {
   state = {
-    selectedFile: null
-  }
+    selectedFile: null,
+  };
 
   fileSelectedHandler = (event) => {
     console.log(event.target.files[0]);
   };
 
   uploadHandler = () => {
-    const formData = new FormData()
+    const formData = new FormData();
     formData.append(
-      'myFile',
+      "myFile",
       this.state.selectedFile,
       this.state.selectedFile.name
-    )
-    axios.post('my-domain.com/file-upload', formData)
-  }
+    );
+    axios.post("my-domain.com/file-upload", formData);
+  };
 
   render() {
     return (
       <>
-        <div className="app">
-          <input
-            className="button"
-            type="file"
-            onChange={this.fileSelectedHandler}
-          />
-          <button className="button is-success" onClick={this.uploadHandler}>
-            Upload
-          </button>
-        </div>
-
-        <div class="card">
-          <header class="card-header">
-            <p class="card-header-title">Component</p>
-            <button class="card-header-icon" aria-label="more options">
-              <span class="icon">
-                <i class="fas fa-angle-down" aria-hidden="true"></i>
-              </span>
-            </button>
-          </header>
-          <div class="card-content">
-            <div class="content">
-              Joao e gay
-              <a href="#">@bulmaio</a>. <a href="#">#css</a>{" "}
-              <a href="#">#responsive</a>
-              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        <main class="app">
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title is-centered">3two1</p>
+              <button class="card-header-icon" aria-label="more options">
+                <span class="icon">
+                  <i class="fas fa-angle-down" aria-hidden="true"></i>
+                </span>
+              </button>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                <div className="app">
+                </div>
+              </div>
             </div>
+            <footer class="card-footer">
+              <a href="#" class="card-footer-item">
+              <input
+                    className="button"
+                    style={{display: 'none'}}
+                    type="file"
+                    onChange={this.fileSelectedHandler}
+                    ref={fileInput => this.fileInput = fileInput}/>
+                <button 
+                className="button"
+                onClick={() => this.fileInput.click()}>Escolher Foto</button>
+              </a>
+              <a href="#" class="card-footer-item">
+                <button
+                  className="button is-success"
+                  onClick={this.uploadHandler}>Upload</button>
+              </a>
+            </footer>
           </div>
-          <footer class="card-footer">
-            <a href="#" class="card-footer-item">
-              Save
-            </a>
-            <a href="#" class="card-footer-item">
-              Edit
-            </a>
-            <a href="#" class="card-footer-item">
-              Delete
-            </a>
-          </footer>
-        </div>
+        </main>
       </>
     );
   }
--- src/testew/imgoverlay.js
+++ src/testew/imgoverlay.js
@@ -1,20 +1,17 @@
-const Jimp = require('jimp') ;
+const Jimp = require("jimp");
 
-async function imageOverlay(imageOverlay) { // Function name is same as of filename
-// Reading watermark Image
-   let watermark = await Jimp.read(imageOverlay);
-  //  watermark = watermark.resize(150,150); // Resizing watermark image
-// Reading original image
-   const image = await Jimp.read('../assets/input.png');
-   watermark = await watermark
-   image.composite(watermark, 0, 0, {
-      mode: Jimp.BLEND_SOURCE_OVER,
-      opacityDest: 1,
-      opacitySource: 1
-   })
-   await image.writeAsync('../assets/output.png');
+async function imageOverlay(imageOverlay) {
+  let watermark = await Jimp.read(imageOverlay);
+  const image = await Jimp.read("../assets/input.png"); // Reading original image
+  watermark = await watermark;
+  image.composite(watermark, 0, 0, {
+    mode: Jimp.BLEND_SOURCE_OVER,
+    opacityDest: 1,
+    opacitySource: 1,
+  });
+  await image.writeAsync("../assets/output.png");
 }
 
 // Calling this function using async
-imageOverlay('../assets/overlay.png');
+imageOverlay("../assets/overlay.png");
 console.log("Image is processed successfully");
