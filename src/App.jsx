aa2cf5cf3886a4bccad4b94dbfc6ca895f938d59
Tiago Henrique Melo Teixeira Netto <thenrique.pitang@ons.org.br>
2021-09-23 10:35:37 -0300

    joao

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,56 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  fileSelectedHandler = event => {
+  fileSelectedHandler = (event) => {
     console.log(event.target.files[0]);
-  }
+  };
 
-  render () {
+  render() {
     return (
-    <div className="app">
-      <input className="button" type="file" onChange={this.fileSelectedHandler}/>
-      <button className="button is-success" onClick={this.uploadHandler}>Upload</button>
-    </div>
-      );
+      <>
+        <div className="app">
+          <input
+            className="button"
+            type="file"
+            onChange={this.fileSelectedHandler}
+          />
+          <button className="button is-success" onClick={this.uploadHandler}>
+            Upload
+          </button>
+        </div>
+
+        <div class="card">
+          <header class="card-header">
+            <p class="card-header-title">Component</p>
+            <button class="card-header-icon" aria-label="more options">
+              <span class="icon">
+                <i class="fas fa-angle-down" aria-hidden="true"></i>
+              </span>
+            </button>
+          </header>
+          <div class="card-content">
+            <div class="content">
+              Joao e gay
+              <a href="#">@bulmaio</a>. <a href="#">#css</a>{" "}
+              <a href="#">#responsive</a>
+              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+            </div>
+          </div>
+          <footer class="card-footer">
+            <a href="#" class="card-footer-item">
+              Save
+            </a>
+            <a href="#" class="card-footer-item">
+              Edit
+            </a>
+            <a href="#" class="card-footer-item">
+              Delete
+            </a>
+          </footer>
+        </div>
+      </>
+    );
   }
 }
 export default App;
